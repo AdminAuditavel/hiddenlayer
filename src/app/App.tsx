5f2fbacaf53d5160d24d1a868e22403e5179a636
AdminAuditavel <admin@auditavel.com>
2026-02-12 18:19:11 -0300

    Fix import paths for view components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { AppView, Product } from "../types";
 import { PRODUCTS } from "../data/products";
 
-import HomeView from "./views/HomeView";
-import CatalogView from "./views/CatalogView";
-import DetailView from "./views/DetailView";
+import HomeView from "../views/HomeView";
+import CatalogView from "../views/CatalogView";
+import DetailView from "../views/DetailView";
 
 import AssistantModal from "./components/AssistantModal";
 import BottomNav from "./components/BottomNav";
