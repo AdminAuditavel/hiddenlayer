ee080255c3c797814b766a07918cb75a261f5b79
AdminAuditavel <admin@auditavel.com>
2026-02-12 18:17:45 -0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AppView, Product } from "./types";
+import { AppView, Product } from "../types";
 import { PRODUCTS } from "../data/products";
 
 import HomeView from "./views/HomeView";
