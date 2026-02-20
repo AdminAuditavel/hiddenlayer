025fb382dc3ad7978b48de369a02976f1c3cd509
AdminAuditavel <admin@auditavel.com>
2026-02-19 22:12:37 -0300

    Update BottomNav.tsx

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -12,10 +12,11 @@ const BottomNav: React.FC<Props> = ({
 }) => {
   return (
     <div className="fixed bottom-0 left-0 right-0 h-20 bg-black/90 backdrop-blur-xl border-t border-white/5 z-50">
-  
+
       <div className="max-w-[1280px] mx-auto px-6 h-full">
         <div className="flex items-center justify-evenly h-full pb-4">
-  
+
+          {/* HOME */}
           <button
             onClick={() => {
               setCurrentView(AppView.HOME);
@@ -29,7 +30,8 @@ const BottomNav: React.FC<Props> = ({
           >
             analytics
           </button>
-  
+
+          {/* CATALOG */}
           <button
             onClick={() => {
               setCurrentView(AppView.CATALOG);
@@ -43,18 +45,30 @@ const BottomNav: React.FC<Props> = ({
           >
             layers
           </button>
-  
-          <button className="material-icons text-white/40">
+
+          {/* FIELD ⭐ */}
+          <button
+            onClick={() => {
+              setCurrentView(AppView.FIELD);
+              window.scrollTo(0, 0);
+            }}
+            className={`material-icons ${
+              currentView === AppView.FIELD
+                ? "text-primary"
+                : "text-white/40"
+            }`}
+          >
             radar
           </button>
-  
+
+          {/* PROFILE / FUTURE */}
           <button className="material-icons text-white/40">
             person_outline
           </button>
-  
+
         </div>
       </div>
-  
+
     </div>
   );
 };
